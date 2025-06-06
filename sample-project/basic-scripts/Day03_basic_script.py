904a3ccba6d0662cd87b8704520a448fdc83304e
saikiran8416 <ksai0650@gmail.com>
2025-06-07 00:03:44 +0530

    fourth day started by revising python basics

--- sample-project/basic-scripts/Day03_basic_script.py
+++
@@ -1,3 +0,0 @@
-# python Basic
-#print
-print("Glad to know that you are view this page")
---
+++ sample-project/basic-scripts/Day03_basic_script_python_basics_1.py
@@ -0,0 +1,15 @@
+# python Basic
+#print
+print("Glad to know that you are view this page")
+
+# print with Arithmetic operations
+print(2-2)
+print(2+2)
+print(2*2)
+print(2/2)
+
+#print with different type of end line parameters
+print("Pramod", 123, "Amit", "John")
+print("Pramod", 123, "Amit", "John", sep='*',end="      ")
+print("Pramod", 123, "Amit", "John", sep='*')
+
