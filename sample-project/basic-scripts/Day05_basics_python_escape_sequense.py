d3b7d2a0f230a88ad944ce60aaa1c29604516c86
saikiran8416 <ksai0650@gmail.com>
2025-06-09 22:23:58 +0530

    5th day of practise

---
+++ sample-project/basic-scripts/Day05_basics_python_escape_sequense.py
@@ -0,0 +1,13 @@
+# Escape Sequence
+print("Hello\nWorld")# \n -> new line after printing Hello
+print("Hello\tWorld") # \t -> tab space after Hello
+print("Hello\bWorld")# \b -> backspace (1 char backspace)
+
+# Where this r concept will be used?
+dir = r'C:\ksai\n.txt'
+dir = r"C:\ksai\t.txt"
+print(dir)
+
+
+print("SAIKIRAN' K\t\ta")
+print(r"KIRAN' K\t\ta")
